4ebdd0b8568efb62f64485a9f34de5eecf823a99
kkunkel <kkunkel@users.noreply.github.com>
2017-06-27 17:38:58 -0400

    Update config.pp
    fixed config_user and config_group to match variable set in param.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,8 +3,8 @@ class osquery::config {
 
   file { $::osquery::config:
     ensure  => present,
-    owner   => $::osquery::conf_owner,
-    group   => $::osquery::conf_group,
+    owner   => $::osquery::config_owner,
+    group   => $::osquery::config_group,
     content => sorted_json($::osquery::settings), # array to JSON lib
     require => Package[$::osquery::package_name],
     notify  => Service[$::osquery::service_name],
