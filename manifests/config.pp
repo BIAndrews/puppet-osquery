bfd538d4b6fc89b6a760b20be3fef1c2c116c522
Jo Rhett <jrhett@netconsonance.com>
2017-12-21 17:19:53 -0800

    Allow optional pretty format of sorted json (#8)
    Add osquery::config::format variable to control output
    Rename configfile variable to avoid name confusion

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,13 @@
 # class osquery::config - manage json config in /etc/osquery
-class osquery::config {
+class osquery::config(
+  $format = 'simple',
+){
 
-  file { $::osquery::config:
+  file { $::osquery::config_file:
     ensure  => present,
+    content => sorted_json($::osquery::settings, $format), # format as JSON
     owner   => $::osquery::config_owner,
     group   => $::osquery::config_group,
-    content => sorted_json($::osquery::settings), # array to JSON lib
     require => Package[$::osquery::package_name],
     notify  => Service[$::osquery::service_name],
   }
